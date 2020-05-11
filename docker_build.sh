dcb3f76a809075dd5c340be08c44bf0c3290d2d5
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-05-11 13:29:33 +0200

    Added wget & unzip

--- docker_build.sh
+++ docker_build.sh
@@ -4,11 +4,10 @@
 ## https://lifehacker.com/start-a-simple-web-server-from-any-directory-on-your-ma-496425450
 # python -m SimpleHTTPServer 8000 &
 # cd $PWD
-#export LOCAL_IP=http://172.20.10.8:8000
+# export LOCAL_IP=http://172.20.10.10:8000
 # export LOCAL_IP=http://192.168.2.100:8000
-#export SWIFT_TF_URL=$LOCAL_IP/swift-tensorflow-RELEASE-0.9-cuda10.2-cudnn7-ubuntu18.04.tar.gz
+# export SWIFT_TF_URL=$LOCAL_IP/swift-tensorflow-RELEASE-0.9-cuda10.2-cudnn7-ubuntu18.04.tar.gz
+# docker build -f docker/Dockerfile -t swift-jupyter --build-arg swift_tf_url=$SWIFT_TF_URL .
 
 # for GCP
 export SWIFT_TF_URL=https://storage.googleapis.com/swift-tensorflow-artifacts/releases/v0.9/rc1/swift-tensorflow-RELEASE-0.9-cuda10.1-cudnn7-ubuntu18.04.tar.gz
-
-docker build -f docker/Dockerfile -t swift-jupyter --build-arg swift_tf_url=$SWIFT_TF_URL .
