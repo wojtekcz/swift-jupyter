8321f40537a711f1888ad6b5d31dcaa0927bdd2e
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-05-11 11:16:43 +0000

    Changes for GCP instance

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,7 @@
-docker run -p 8888:8888 --cap-add SYS_PTRACE -v $(pwd)/..:/notebooks --name swift-jupyter swift-jupyter
+# for GCP
+cd ~
+docker run --gpus all --name swift-jupyter -p 8081:8888 --privileged -v $(pwd):/notebooks -v swift-jupyter swift-jupyter
+
+# locally
+#export PORT1=8888:8888
+# docker run -p $PORT1 --cap-add SYS_PTRACE -v $(pwd)/..:/notebooks --name swift-jupyter swift-jupyter
