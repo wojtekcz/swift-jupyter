7dea53e6edc94077f65941faa301a82dc7bd78bd
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-05-11 14:17:48 +0200

    Differentiate between local and GCP machines

--- docker_build.sh
+++ docker_build.sh
@@ -1,13 +1,21 @@
-# for local
-# PWD=pwd
-# cd ..
-## https://lifehacker.com/start-a-simple-web-server-from-any-directory-on-your-ma-496425450
-# python -m SimpleHTTPServer 8000 &
-# cd $PWD
-# export LOCAL_IP=http://172.20.10.10:8000
-# export LOCAL_IP=http://192.168.2.100:8000
-# export SWIFT_TF_URL=$LOCAL_IP/swift-tensorflow-RELEASE-0.9-cuda10.2-cudnn7-ubuntu18.04.tar.gz
-# docker build -f docker/Dockerfile -t swift-jupyter --build-arg swift_tf_url=$SWIFT_TF_URL .
+# docker_build.sh gcp|local
+# build docker image on GCP instance or locally
 
-# for GCP
-export SWIFT_TF_URL=https://storage.googleapis.com/swift-tensorflow-artifacts/releases/v0.9/rc1/swift-tensorflow-RELEASE-0.9-cuda10.1-cudnn7-ubuntu18.04.tar.gz
+if [ "$1" == "local" ]; then
+    # assuming swift toolchain tgz file is downloaded
+    cd ..
+    # https://lifehacker.com/start-a-simple-web-server-from-any-directory-on-your-ma-496425450
+    python -m SimpleHTTPServer 8000 &
+    cd swift-jupyter.gt
+
+    # LOCAL_IP for http server, can't be localhost
+    export LOCAL_IP=http://172.20.10.10:8000
+    # export LOCAL_IP=http://192.168.2.100:8000
+    export SWIFT_TF_URL=$LOCAL_IP/swift-tensorflow-RELEASE-0.9-cuda10.2-cudnn7-ubuntu18.04.tar.gz
+    docker build -f docker/Dockerfile -t swift-jupyter --build-arg swift_tf_url=$SWIFT_TF_URL .
+fi
+
+if [ "$1" == "gcp" ]; then
+    export SWIFT_TF_URL=https://storage.googleapis.com/swift-tensorflow-artifacts/releases/v0.9/rc1/swift-tensorflow-RELEASE-0.9-cuda10.1-cudnn7-ubuntu18.04.tar.gz
+    docker build -f docker/Dockerfile -t swift-jupyter --build-arg swift_tf_url=$SWIFT_TF_URL .
+fi
--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,12 @@
-# for GCP
-cd ~
-docker run --gpus all --name swift-jupyter -p 8081:8888 --privileged -v $(pwd):/notebooks -v swift-jupyter swift-jupyter
+# docker_run.sh gcp|local
+# create container on GCP instance or locally
 
-# locally
-#export PORT1=8888:8888
-# docker run -p $PORT1 --cap-add SYS_PTRACE -v $(pwd)/..:/notebooks --name swift-jupyter swift-jupyter
+if [ "$1" == "gcp" ]; then
+    cd ~
+    docker run --gpus all --name swift-jupyter -p 8081:8888 --privileged -v $(pwd):/notebooks -v swift-jupyter swift-jupyter
+fi
+
+if [ "$1" == "local" ]; then
+    export PORT1=8888:8888
+    docker run -p $PORT1 --cap-add SYS_PTRACE -v $(pwd)/..:/notebooks --name swift-jupyter swift-jupyter
+fi
