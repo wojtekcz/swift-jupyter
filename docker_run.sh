31d1517d464727aca31e1d004ae9ab50d031098b
Wojciech Czarnowski <wcz@jatar.com.pl>
2020-06-25 11:44:02 +0200

    Streamlined docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,15 +1,20 @@
-# docker_run.sh gpu|cpu_macos
+# docker_run.sh gpu|macos
 # create container on GPU machine or on macOS
 
 export PORT1=8888:8888 # jupyter lab
 export PORT2=6006:6006 # tensorboard
 # export PORT1=8081:8888  # for Wojtek's GCP
 
+GPU_opts=""
+macOS_opts=""
+
 if [ "$1" == "gpu" ]; then
-    cd ~
-    docker run --name swift-jupyter -p $PORT1 -p $PORT2 --gpus all --privileged -v $(pwd):/notebooks swift-jupyter
+    GPU_opts=--gpus all
 fi
 
-if [ "$1" == "cpu_macos" ]; then
-    docker run --name swift-jupyter -p $PORT1 -p $PORT2 --cap-add SYS_PTRACE -v $(pwd)/..:/notebooks swift-jupyter
+if [ "$1" == "macos" ]; then
+    macOS_opts="--cap-add SYS_PTRACE"
 fi
+
+cd ..
+docker run --name swift-jupyter -p $PORT1 -p $PORT2 $GPU_opts --privileged $macOS_opts -v $(pwd):/notebooks swift-jupyter
